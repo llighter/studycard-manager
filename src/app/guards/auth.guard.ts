b069db15073ec080af181a34e922134d1612a049
llighter <hadbsgk@gmail.com>
2020-08-17 11:07:10 +0900

    Generate Guard: auth guard(#8)

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router
+    , private auth: AuthService
+  ) { }
+
+  async canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Promise<boolean> {
+      const uid = await this.auth.uid();
+      const isLoggedIn = !!uid;
+
+      if (!isLoggedIn) {
+        this.router.navigate(['login']);
+      }
+
+      return isLoggedIn;
+  }
+  
+}
