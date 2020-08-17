c0fed3fbddcd8aab4237e37bfed2c3d594797560
llighter <hadbsgk@gmail.com>
2020-08-17 09:40:05 +0900

    Setup firebase: app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    BrowserModule
+    , AppRoutingModule
+    , BrowserAnimationsModule
+    , AngularFireModule.initializeApp(environment.firebaseConfig)
+    , AngularFireAuthModule
+    , AngularFirestoreModule
+    , ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
